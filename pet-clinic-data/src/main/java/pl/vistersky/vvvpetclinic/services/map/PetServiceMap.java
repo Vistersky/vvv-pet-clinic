79d572626273d02ea8030337e7fea39981010e29
vistersky <vistersky@gmail.com>
2018-12-03 04:47:02 +0100

    Implement Map based Services. Closes #6.

---
+++ pet-clinic-data/src/main/java/pl/vistersky/vvvpetclinic/services/map/PetServiceMap.java
@@ -0,0 +1,34 @@
+package pl.vistersky.vvvpetclinic.services.map;
+
+import pl.vistersky.vvvpetclinic.model.Pet;
+import pl.vistersky.vvvpetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
