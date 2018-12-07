91782f7ea8f6b8904549c126c1bef68f0c543abb
vistersky <vistersky@gmail.com>
2018-12-07 14:42:30 +0100

    Added Specialities, added to vet. Closes #27.

---
+++ pet-clinic-data/src/main/java/pl/vistersky/vvvpetclinic/model/Speciality.java
@@ -0,0 +1,14 @@
+package pl.vistersky.vvvpetclinic.model;
+
+public class Speciality {
+
+    private String desc;
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+}
