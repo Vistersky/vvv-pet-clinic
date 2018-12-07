a93ca8363bda28ab59e0b5a7d8aba4305e8c293f
vistersky <vistersky@gmail.com>
2018-12-07 19:41:42 +0100

    Updated Vet to JPA. Closes #37.

--- pet-clinic-data/src/main/java/pl/vistersky/vvvpetclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/pl/vistersky/vvvpetclinic/model/Speciality.java
@@ -1,7 +1,14 @@
 package pl.vistersky.vvvpetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "specialities")
 public class Speciality extends BaseEntity{
 
+    @Column(name = "desc")
     private String desc;
 
     public String getDesc() {
