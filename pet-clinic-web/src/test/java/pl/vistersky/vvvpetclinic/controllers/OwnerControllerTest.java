8abc071a61e35e40345a26703726f12a7d18690c
vistersky <vistersky@gmail.com>
2018-12-09 13:09:01 +0100

    Problem with show owner

--- pet-clinic-web/src/test/java/pl/vistersky/vvvpetclinic/controllers/OwnerControllerTest.java
+++ pet-clinic-web/src/test/java/pl/vistersky/vvvpetclinic/controllers/OwnerControllerTest.java
@@ -14,7 +14,10 @@ import pl.vistersky.vvvpetclinic.services.OwnerService;
 import java.util.HashSet;
 import java.util.Set;
 
+import static org.hamcrest.Matchers.hasProperty;
 import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.verifyZeroInteractions;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -57,6 +60,16 @@ class OwnerControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(view().name("owners/index"))
                 .andExpect(model().attribute("owners", hasSize(2)));
+    }
+
+
+    @Test
+    void displayOwner() throws Exception {
+        when(ownerService.findById(anyLong())).thenReturn(Owner.builder().id(1L).build());
+        mockMVC.perform(get("/owners/123"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("owners/ownerDetails"))
+                .andExpect(model().attribute("owner", hasProperty("id", is(1L))));
     }
 
     @Test
